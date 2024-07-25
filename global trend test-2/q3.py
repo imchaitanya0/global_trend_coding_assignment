0da2c69cd2c4921a0f0097fcfd8fadfba97ef572
Chaitanya kadupukutla <108838076+imchaitanya0@users.noreply.github.com>
2024-07-25 17:10:57 +0530

    Update q3.py

---
+++ global trend test-2/q3.py
@@ -0,0 +1,5 @@
+''''
+Write a function to solve the 0/1 Knapsack problem using dynamic programming.
+Sample Test Case
+Input: weights = [1, 2, 3], values = [10, 15, 40], capacity = 6 Output: 55 (Maximum value that can be obtained)
+'''
